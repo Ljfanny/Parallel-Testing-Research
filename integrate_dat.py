6d71317e6e873e7f929df54ce7480b30ec8f81c8
Ljfanny <84827635+Ljfanny@users.noreply.github.com>
2023-08-30 18:37:48 +0800

    Add plots

--- integrate_dat.py
+++ integrate_dat.py
@@ -2,84 +2,133 @@ import os
 from ast import literal_eval
 import pandas as pd
 
-choice = 2
 category_idx = 0
 confs_idx = 2
 time_idx = 4
 price_idx = 5
-csv_names = [
-    'table_for_0.csv',
-    'table_for_0.2.csv',
-    'table_for_0.4.csv',
-    'table_for_0.6.csv',
-    'table_for_0.8.csv',
-    'table_for_1.csv']
-frs = [0, 0.2, 0.4, 0.6, 0.8, 1]
-dfs = [pd.DataFrame(None,
-                    columns=['project',
-                             'cheapest_category',
-                             'cheapest_confs',
-                             'cheapest_runtime',
-                             'cheapest_price',
-                             'cheapest_github_caliber_confs',
-                             'cheapest_github_caliber_runtime',
-                             'cheapest_github_caliber_price',
-                             'cheapest_github_caliber_runtime_rate',
-                             'cheapest_github_caliber_price_saving',
-                             'cheapest_smart_baseline_confs',
-                             'cheapest_smart_baseline_runtime',
-                             'cheapest_smart_baseline_price',
-                             'cheapest_smart_baseline_runtime_rate',
-                             'cheapest_smart_baseline_price_saving',
-                             'fastest_category',
-                             'fastest_confs',
-                             'fastest_runtime',
-                             'fastest_price',
-                             'fastest_github_caliber_confs',
-                             'fastest_github_caliber_runtime',
-                             'fastest_github_caliber_price',
-                             'fastest_github_caliber_runtime_rate',
-                             'fastest_github_caliber_price_saving',
-                             'fastest_smart_baseline_confs',
-                             'fastest_smart_baseline_runtime',
-                             'fastest_smart_baseline_price',
-                             'fastest_smart_baseline_runtime_rate',
-                             'fastest_smart_baseline_price_saving']) for _ in range(6)]
+max_fr_idx = 7
 alter_conf = '27CPU2Mem8GB.sh'
-fr_idx_map = {0: 0, 0.2: 1, 0.4: 2, 0.6: 3, 0.8: 4, 1: 5}
 bl_paths = ['baseline_dat/incl_cost/', 'baseline_dat/incl_cost/', 'baseline_dat/excl_cost/']
 dat_paths = ['bruteforce_dat/', 'ext_dat/incl_cost/', 'ext_dat/excl_cost/']
 outputs = ['integration_dat_bruteforce/', 'integration_dat_incl_cost/', 'integration_dat_excl_cost/']
 
 
-def get_contrast(proj: str, mach_num: int, fr: float):
+def record_df(df, proj_name, chp, chp_gh, chp_smt, fst, fst_gh, fst_smt):
+    chp_time = chp[time_idx]
+    chp_price = chp[price_idx]
+    fst_time = fst[time_idx]
+    fst_price = fst[price_idx]
+    df.loc[len(df.index)] = [
+        proj_name,
+        chp[category_idx],
+        chp[confs_idx],
+        chp_time,
+        chp_price,
+        chp[max_fr_idx],
+        chp_gh[confs_idx],
+        chp_gh[time_idx],
+        chp_gh[price_idx],
+        chp_gh[max_fr_idx],
+        chp_time / chp_gh[time_idx],
+        1 - chp_price / chp_gh[price_idx],
+        chp_smt[confs_idx],
+        chp_smt[time_idx],
+        chp_smt[price_idx],
+        chp_smt[max_fr_idx],
+        chp_time / chp_smt[time_idx],
+        1 - chp_price / chp_smt[price_idx],
+        fst[category_idx],
+        fst[confs_idx],
+        fst_time,
+        fst_price,
+        fst[max_fr_idx],
+        fst_gh[confs_idx],
+        fst_gh[time_idx],
+        fst_gh[price_idx],
+        fst_gh[max_fr_idx],
+        1 - fst_time / fst_gh[time_idx],
+        fst_price / fst_gh[price_idx],
+        fst_smt[confs_idx],
+        fst_smt[time_idx],
+        fst_smt[price_idx],
+        fst_smt[max_fr_idx],
+        1 - fst_time / fst_smt[time_idx],
+        fst_price / fst_smt[price_idx]
+    ]
+
+
+def get_contrast(choice: int, proj: str, mach_num: int, is_fr=False, fr=None):
     baseline = bl_paths[choice] + proj + '.csv'
     dat = pd.read_csv(baseline)
-    cond = str(mach_num) + '-' + str(fr) + '-'
-    filter_dat = dat.loc[dat['machine_list_or_failure_rate_or_cheap_or_fast_category'].str.contains(cond)]
+    cond = str(mach_num) + '-'
+    if is_fr:
+        cond += str(fr) + '-'
+    filter_dat = dat.loc[dat['machine_list_or_failure_rate_or_cheap_or_fast_category'].str.find(cond) == 0]
     filter_dat = (filter_dat.iloc[:, 1:]).reset_index(drop=True)
     github_caliber = filter_dat.loc[filter_dat['confs'].str.contains(alter_conf)].iloc[0]
     filter_dat.sort_values(by='max_failure_rate', inplace=True)
     return github_caliber, filter_dat.iloc[0, :]
 
 
-if __name__ == '__main__':
+def consider_fr(choice: int):
+    csv_names = [
+        'table_for_0.csv',
+        'table_for_0.2.csv',
+        'table_for_0.4.csv',
+        'table_for_0.6.csv',
+        'table_for_0.8.csv',
+        'table_for_1.csv']
+    frs = [0, 0.2, 0.4, 0.6, 0.8, 1]
+    dfs = [pd.DataFrame(None, columns=['project',
+                                       'cheapest_category',
+                                       'cheapest_confs',
+                                       'cheapest_runtime',
+                                       'cheapest_price',
+                                       'cheapest_max_failure_rate',
+                                       'cheapest_github_caliber_confs',
+                                       'cheapest_github_caliber_runtime',
+                                       'cheapest_github_caliber_price',
+                                       'cheapest_github_caliber_max_failure_rate',
+                                       'cheapest_github_caliber_runtime_rate',
+                                       'cheapest_github_caliber_price_saving',
+                                       'cheapest_smart_baseline_confs',
+                                       'cheapest_smart_baseline_runtime',
+                                       'cheapest_smart_baseline_price',
+                                       'cheapest_smart_baseline_max_failure_rate',
+                                       'cheapest_smart_baseline_runtime_rate',
+                                       'cheapest_smart_baseline_price_saving',
+                                       'fastest_category',
+                                       'fastest_confs',
+                                       'fastest_runtime',
+                                       'fastest_price',
+                                       'fastest_max_failure_rate',
+                                       'fastest_github_caliber_confs',
+                                       'fastest_github_caliber_runtime',
+                                       'fastest_github_caliber_price',
+                                       'fastest_github_caliber_max_failure_rate',
+                                       'fastest_github_caliber_runtime_rate',
+                                       'fastest_github_caliber_price_saving',
+                                       'fastest_smart_baseline_confs',
+                                       'fastest_smart_baseline_runtime',
+                                       'fastest_smart_baseline_price',
+                                       'fastest_smart_baseline_max_failure_rate',
+                                       'fastest_smart_baseline_runtime_rate',
+                                       'fastest_smart_baseline_price_saving']) for _ in range(6)]
+    fr_idx_map = {0: 0, 0.2: 1, 0.4: 2, 0.6: 3, 0.8: 4, 1: 5}
     dat_path = dat_paths[choice]
     output = outputs[choice]
     filenames = os.listdir(dat_path)
     for f in filenames:
         proj_name = f[:f.index('csv') - 1]
-        df = pd.read_csv(dat_path + f)
-        arr = df.iloc[:, 1:].values
+        dat = pd.read_csv(dat_path + f)
+        arr = dat.iloc[:, 1:].values
         fr_cond_arr = [[] for _ in range(6)]
         for itm in arr:
             idx = fr_idx_map[float(itm[0].split('-')[1])]
             fr_cond_arr[idx].append(itm)
         for i, fr_cond in enumerate(fr_cond_arr):
-            chp_cate = ''
-            fst_cate = ''
-            chp_conf = None
-            fst_conf = None
+            chp = None
+            fst = None
             chp_sort = sorted(fr_cond, key=lambda x: x[price_idx])
             fst_sort = sorted(fr_cond, key=lambda x: x[time_idx])
             fst_time = fst_sort[0][time_idx]
@@ -89,45 +138,93 @@ if __name__ == '__main__':
             for itm in chp_sort:
                 if itm[price_idx] == chp_price:
                     if chp_time > itm[time_idx]:
+                        chp = itm
                         chp_time = itm[time_idx]
-                        chp_conf = itm[confs_idx]
-                        chp_cate = itm[category_idx]
             for itm in fst_sort:
                 if itm[time_idx] == fst_time:
                     if fst_price > itm[price_idx]:
+                        fst = itm
                         fst_price = itm[price_idx]
-                        fst_conf = itm[confs_idx]
-                        fst_cate = itm[category_idx]
-            chp_gh, chp_smt = get_contrast(proj_name, sum(literal_eval(chp_conf).values()), frs[i])
-            fst_gh, fst_smt = get_contrast(proj_name, sum(literal_eval(fst_conf).values()), frs[i])
-            dfs[i].loc[len(dfs[i].index)] = [proj_name,
-                                             chp_cate,
-                                             chp_conf,
-                                             chp_time,
-                                             chp_price,
-                                             chp_gh[confs_idx],
-                                             chp_gh[time_idx],
-                                             chp_gh[price_idx],
-                                             chp_time / chp_gh[time_idx],
-                                             1 - chp_price / chp_gh[price_idx],
-                                             chp_smt[confs_idx],
-                                             chp_smt[time_idx],
-                                             chp_smt[price_idx],
-                                             chp_time / chp_smt[time_idx],
-                                             1 - chp_price / chp_smt[price_idx],
-                                             fst_cate,
-                                             fst_conf,
-                                             fst_time,
-                                             fst_price,
-                                             fst_gh[confs_idx],
-                                             fst_gh[time_idx],
-                                             fst_gh[price_idx],
-                                             1 - fst_time / fst_gh[time_idx],
-                                             fst_price / fst_gh[price_idx],
-                                             fst_smt[confs_idx],
-                                             fst_smt[time_idx],
-                                             fst_smt[price_idx],
-                                             1 - fst_time / fst_smt[time_idx],
-                                             fst_price / fst_smt[price_idx]]
+            chp_gh, chp_smt = get_contrast(choice, proj_name, sum(literal_eval(chp[confs_idx]).values()), True, frs[i])
+            fst_gh, fst_smt = get_contrast(choice, proj_name, sum(literal_eval(fst[confs_idx]).values()), True, frs[i])
+            record_df(dfs[i], proj_name, chp, chp_gh, chp_smt, fst, fst_gh, fst_smt)
     for i, df in enumerate(dfs):
         df.to_csv(output + csv_names[i], sep=',', float_format='%.2f', header=True, index=False)
+
+
+# only consider GA method
+def ignore_fr():
+    df = pd.DataFrame(None, columns=['project',
+                                       'cheapest_category',
+                                       'cheapest_confs',
+                                       'cheapest_runtime',
+                                       'cheapest_price',
+                                       'cheapest_max_failure_rate',
+                                       'cheapest_github_caliber_confs',
+                                       'cheapest_github_caliber_runtime',
+                                       'cheapest_github_caliber_price',
+                                       'cheapest_github_caliber_max_failure_rate',
+                                       'cheapest_github_caliber_runtime_rate',
+                                       'cheapest_github_caliber_price_saving',
+                                       'cheapest_smart_baseline_confs',
+                                       'cheapest_smart_baseline_runtime',
+                                       'cheapest_smart_baseline_price',
+                                       'cheapest_smart_baseline_max_failure_rate',
+                                       'cheapest_smart_baseline_runtime_rate',
+                                       'cheapest_smart_baseline_price_saving',
+                                       'fastest_category',
+                                       'fastest_confs',
+                                       'fastest_runtime',
+                                       'fastest_price',
+                                       'fastest_max_failure_rate',
+                                       'fastest_github_caliber_confs',
+                                       'fastest_github_caliber_runtime',
+                                       'fastest_github_caliber_price',
+                                       'fastest_github_caliber_max_failure_rate',
+                                       'fastest_github_caliber_runtime_rate',
+                                       'fastest_github_caliber_price_saving',
+                                       'fastest_smart_baseline_confs',
+                                       'fastest_smart_baseline_runtime',
+                                       'fastest_smart_baseline_price',
+                                       'fastest_smart_baseline_max_failure_rate',
+                                       'fastest_smart_baseline_runtime_rate',
+                                       'fastest_smart_baseline_price_saving'])
+    dat_path = dat_paths[1]
+    filenames = os.listdir(dat_path)
+    for f in filenames:
+        proj_name = f.replace('.csv', '')
+        dat = pd.read_csv(dat_path + f).iloc[:, 1:]
+        dat.sort_values(by='time_parallel', inplace=True)
+        fst = dat.iloc[0, :]
+        fst_time = fst[time_idx]
+        fst_price = fst[price_idx]
+        for _, row in dat.iterrows():
+            if row[time_idx] == fst_time:
+                if row[price_idx] < fst_price:
+                    fst = row
+                    fst_price = row[price_idx]
+            else:
+                break
+        dat.sort_values(by='price', inplace=True)
+        chp = None
+        chp_time = float('inf')
+        chp_price = dat.iloc[0, price_idx]
+        for _, row in dat.iterrows():
+            if row[price_idx] == chp_price:
+                if row[time_idx] < chp_time:
+                    chp = row
+                    chp_time = row[time_idx]
+            else:
+                break
+        chp_gh, chp_smt = get_contrast(1, proj_name, sum(literal_eval(chp[confs_idx]).values()))
+        fst_gh, fst_smt = get_contrast(1, proj_name, sum(literal_eval(fst[confs_idx]).values()))
+        record_df(df, proj_name, chp, chp_gh, chp_smt, fst, fst_gh, fst_smt)
+    df.to_csv('integration_dat_ga.csv', sep=',', float_format='%.2f', header=True, index=False)
+
+
+if __name__ == '__main__':
+    modus = {'bruteforce': 0, 'incl': 1, 'excl': 2}
+    consider_fr(modus['bruteforce'])
+    consider_fr(modus['incl'])
+    consider_fr(modus['excl'])
+    ignore_fr()
--- plotter.py
+++ plotter.py
@@ -1,44 +1,92 @@
 import os
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import textwrap
+import numpy as np
+
+pareto_path = 'pareto'
+trade_off_path = 'trade_off'
 
 
-def pareto_runtime_price(dat_path: str):
+def pareto_runtime_price(dat_name: str):
+    proj_idx = 0
+    colors = ['mediumpurple', 'thistle', 'lightsteelblue', 'darkseagreen', 'slategrey', 'darkkhaki']
+    biases = {'chp': 0, 'chp_gh': 4, 'chp_smt': 10, 'fst': 17, 'fst_gh': 21, 'fst_smt': 27}
+    nodes = {'chp': 'cheapest', 'chp_gh': 'cheapest_github_caliber', 'chp_smt': 'cheapest_smart_baseline',
+             'fst': 'fastest', 'fst_gh': 'fastest_github_caliber', 'fst_smt': 'fastest_smart_baseline'}
     confs_idx = 2
-    runtime_idx = 4
-    price_idx = 5
-    max_fr_idx = 7
-    plt.figure(figsize=(20, 40))
-    csvs = os.listdir(dat_path)
-    for i, csv in enumerate(csvs):
-        subplot = plt.subplot(8, 4, i + 1)
-        subplot.set_title(csv.replace('.csv', ''))
-        # # (runtime,price): (max failure rate<minimum>,confs)
-        mp = dict()
-        df = pd.read_csv(dat_path + csv).iloc[:, 1:]
+    runtime_idx = 3
+    price_idx = 4
+    max_fr_idx = 5
+    dat = pd.read_csv(dat_name)
+    for _, row in dat.iterrows():
+        proj_name = row[proj_idx]
+        x_runtime = []
+        y_price = []
+        max_frs = []
+        annotations = []
+        for key, bias in biases.items():
+            x_runtime.append(row[runtime_idx + bias])
+            y_price.append(row[price_idx + bias])
+            max_frs.append(row[max_fr_idx + bias])
+            annotations.append(nodes[key])
+        plt.scatter(x_runtime, y_price, s=75, c=colors)
+        plt.title(textwrap.fill(proj_name), fontsize=14)
+        plt.xlabel('Runtime')
+        plt.ylabel('Price')
+        plt.gca().invert_xaxis()
+        plt.gca().invert_yaxis()
+        plt.legend(handles=[plt.scatter([], [], c=c) for c in colors],
+                   labels=annotations)
+        # for x, y, fr in zip(x_runtime, y_price, max_frs):
+        #     plt.text(x, y, round(fr, 2), ha='center')
+        plt.savefig(f'{pareto_path}/{proj_name}.png', dpi=1000)
+        plt.close()
+
+
+def draw_trade_off():
+    def get_x_y(df):
+        # (runtime, price): (max failure rate, confs)
+        mp = {}
+        confs_idx = 2
+        runtime_idx = 4
+        price_idx = 5
+        max_fr_idx = 7
         for _, row in df.iterrows():
             key = (row[runtime_idx], row[price_idx])
-            if key in mp.keys():
-                max_fr = mp[key][0]
-                if max_fr > row[max_fr_idx]:
-                    mp[key] = (row[max_fr_idx], row[confs_idx])
-            else:
+            if key not in mp.keys() or row[max_fr_idx] < mp[key][0]:
                 mp[key] = (row[max_fr_idx], row[confs_idx])
         keys = np.array(list(mp.keys()))
-        x_runtimes = keys[:, 0]
-        y_prices = keys[:, 1]
-        subplot.plot(x_runtimes, y_prices, 'o')
+        return keys[:, 0], keys[:, 1]
+
+    def draw(subplot, x, y, title):
+        colors = np.random.rand(len(x))
+        subplot.scatter(x, y, c=colors)
+        subplot.set_title(title)
         subplot.set_xlabel('Runtime')
-        x_bias = (np.min(x_runtimes)+np.max(x_runtimes))/2
-        y_bias = (np.min(y_prices)+np.max(y_prices))/2
-        subplot.set_xlim(np.min(x_runtimes)-x_bias, np.max(x_runtimes)+x_bias)
         subplot.set_ylabel('Price')
-        subplot.set_ylim(np.min(y_prices)-y_bias, np.max(y_prices)+y_bias)
-        # for x, y in zip(x_runtimes, y_prices):
-        #     subplot.text(x, y, round(mp[(x, y)][0], 2), ha='center')
-    plt.show()
+        subplot.invert_xaxis()
+        subplot.invert_yaxis()
+
+    incl_path = 'ext_dat/incl_cost/'
+    csvs = os.listdir(incl_path)
+    category_column_name = 'machine_list_or_failure_rate_or_cheap_or_fast_category'
+    for csv in csvs:
+        proj_name = csv.replace('.csv', '')
+        dat = pd.read_csv(incl_path + csv).iloc[:, 1:]
+        fig = plt.figure(figsize=(10, 4.5))
+        fig.suptitle(proj_name, fontsize=14)
+        chp_df = dat.loc[dat[category_column_name].str.contains('cheap')]
+        fst_df = dat.loc[dat[category_column_name].str.contains('fast')]
+        chp_x, chp_y = get_x_y(chp_df)
+        fst_x, fst_y = get_x_y(fst_df)
+        draw(fig.add_subplot(121), chp_x, chp_y, 'cheap')
+        draw(fig.add_subplot(122), fst_x, fst_y, 'fast')
+        plt.subplots_adjust(wspace=0.3)
+        fig.savefig(f'{trade_off_path}/{proj_name}.png', dpi=1000)
+        plt.close()
 
 
 if __name__ == '__main__':
-    pareto_runtime_price('ext_dat/incl_cost/')
+    # pareto_runtime_price('integration_dat_ga.csv')
+    draw_trade_off()
